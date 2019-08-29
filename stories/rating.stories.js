c028e262e3ec264cfbd7d5a7cee063db824f69aa
henning <henning.nobbe@innoq.com>
2019-08-29 17:07:28 +0200

    try to apply style on storybook after deployment

--- stories/rating.stories.js
+++ stories/rating.stories.js
@@ -6,4 +6,4 @@ const rating = '<wg-rating></wg-rating>';
 storiesOf('Components|Rating', module)
     .add('dummy-values', () => '<wg-rating dummy-text="9999 Google Rezensionen" dummy-uri="example.com" dummy-rating="3.5"></wg-rating>')
     .add('fetched-values', () => rating)
-    .add('external-stylesheet', () => '<wg-rating styling="rating.css"></wg-rating>');
+    .add('external-stylesheet', () => '<wg-rating styling="./rating.css"></wg-rating>');

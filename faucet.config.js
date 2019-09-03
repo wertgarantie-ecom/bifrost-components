ef765bce783453ad69cca00633ea7e051ee5574c
arnel <arne.landwehr@innoq.com>
2019-09-03 13:17:43 +0200

    midgard-#4 transpile JS on every build

--- faucet.config.js
+++ faucet.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     js: [{
         source: "./components/rating/rating.js",
-        target: "./public/rating.js"
+        target: "./public/rating.js",
+        esnext: true
     }],
     manifest: {
         target: "./public/manifest.json"

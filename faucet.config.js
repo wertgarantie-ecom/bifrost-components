7ac7daa337bd2335b42d3362f7e2ffa6671ece8c
arnel <arne.landwehr@innoq.com>
2019-09-03 13:43:08 +0200

    minify JS (Fixes #5)

--- faucet.config.js
+++ faucet.config.js
@@ -3,6 +3,10 @@ module.exports = {
         source: "./components/rating/rating.js",
         target: "./public/rating.js",
         esnext: true
+    }, {
+        source: "./components/rating/rating.js",
+        target: "./public/rating.min.js",
+        compact: "mangle"
     }],
     manifest: {
         target: "./public/manifest.json"

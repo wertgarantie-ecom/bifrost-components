815db68f0bb531dad99e41d999601dc2f78ce174
henning <henning.nobbe@innoq.com>
2019-10-31 16:23:35 +0100

    Try to fix build

--- packages/package-rating/tests/ratingTest.js
+++ packages/package-rating/tests/ratingTest.js
@@ -5,11 +5,8 @@ module.exports = {
             .url("file://" + process.cwd() + "/packages/package-rating/testPages/ratingTest.html")
             .waitForElementVisible('#rating-fetch-success')
             .expect.element('#rating-fetch-success').to.be.visible;
-        
-    }
-}
 
-module.exports = {
+    },
     "Should not display rating component with incomplete data": function(browser) {
         browser
             .url("file://" + process.cwd() + "/packages/package-rating/testPages/ratingTest.html")

8db959f3a7a0b48d4b3223a06cc2d9ad50019e5c
henning <henning.nobbe@innoq.com>
2020-02-13 13:22:36 +0100

    lit-element make rating component work

--- packages/package-rating/faucet.config.js
+++ packages/package-rating/faucet.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    js: [{
-        source: "./src/rating.js",
-        target: "./dist/rating.min.js",
-        compact: "mangle"
-    }],
+    js: [
+        {
+            source: "./src/rating.js",
+            target: "./dist/rating.min.js",
+            compact: "mangle"
+        },
+        {
+            source: "./src/rating-lit.js",
+            target: "./dist/rating-lit.min.js",
+            compact: "mangle"
+        }],
     watchDirs: ["./*"]
 };
--- packages/package-rating/src/rating-lit.js
+++ packages/package-rating/src/rating-lit.js
@@ -1,40 +1,49 @@
-import {LitElement, html, css} from 'lit-element';
+import {LitElement, html} from 'lit-element';
+import {ratingLitStyling} from './rating-lit-styling';
 
 const starText = '★★★★★';
 
 class WertgarantieRating extends LitElement {
     static get properties() {
         return {
-            "rating": {type: Number},
-            "link": {type: String},
-            "linkText": {type: String},
-            "bifrostUri": {type: String},
-            "ratingsTotal": {type: Number},
-            "showRatingNumber": {type: Boolean}
+            "data-rating": {type: Number},
+            "data-link": {type: String},
+            "data-linkText": {type: String},
+            "data-bifrost-uri": {type: String},
+            "data-ratingsTotal": {type: Number},
+            "data-show-rating-number": {type: Boolean}
         };
+    }
+
+    static get styles() {
+        return ratingLitStyling;
     }
 
     constructor() {
         super();
-        this.bifrostUri = "https://wertgarantie-bifrost-dev.herokuapp.com/wertgarantie";
-        this.fetchRating(this.bifrostUri + '/rating')
+        this["data-bifrost-uri"] = "https://wertgarantie-bifrost-dev.herokuapp.com/wertgarantie";
+        this.fetchRating = this.fetchRating.bind(this);
+        this.checkIfRatingDefined = this.checkIfRatingDefined.bind(this);
+        this.setProperties = this.setProperties.bind(this);
+        this.allDisplayDataAvailable = this.allDisplayDataAvailable.bind(this);
+        this.fetchRating(this["data-bifrost-uri"] + '/rating')
             .then(this.checkIfRatingDefined)
             .then(this.setProperties)
     }
 
     render() {
-        return html` <div class="rating">
-            ${this.showRatingNumber ? html`<span class="rating__number" id="rating">${this.rating}</span>` : html``}
-            <div class="rating__stars" id="wertgarantie-rating-stars" style="--rating:${this.rating};">★★★★★</div>
-            <a target="_blank" class="rating__link" href="${this.link}">${this.ratingsTotal} ${this.linkText}</a>
+        return html`<div class="rating">
+            ${this["data-show-rating-number"] ? html`<span class="rating__number" id="rating">${this["data-rating"]}</span>` : html``}
+            <div class="rating__stars" id="wertgarantie-rating-stars" style="--rating:${this["data-rating"]};">${starText}</div>
+            <a target="_blank" class="rating__link" href="${this["data-link"]}">${this["data-ratingsTotal"] + " " + this["data-linkText"]}</a>
         </div>`;
     }
 
     setProperties(ratingResponse) {
-        this.rating = Math.round(ratingResponse.rating * 10) / 10;
-        this.link = ratingResponse.uri;
-        this.linkText = ratingResponse.text;
-        this.ratingsTotal = ratingResponse.ratingsTotal;
+        this["data-rating"] = Math.round(ratingResponse.rating * 10) / 10;
+        this["data-link"] = ratingResponse.uri;
+        this["data-linkText"] = ratingResponse.text;
+        this["data-ratingsTotal"] = ratingResponse.ratingsTotal;
     }
 
     allDisplayDataAvailable(displayData) {
@@ -74,6 +83,7 @@ class WertgarantieRating extends LitElement {
     }
 
     checkIfRatingDefined(displayData) {
+        console.log(displayData);
         if (!this.allDisplayDataAvailable(displayData)) {
             this.remove();
             throw new Error("display data incomplete");
@@ -81,62 +91,6 @@ class WertgarantieRating extends LitElement {
             return displayData;
         }
     }
-
-    updateDisplay({rating, uri, ratingsTotal, text, showRatingNumber}) {
-        rating = Math.round(rating * 10) / 10;
-        this.ratingStarsDiv.innerText = '★★★★★';
-        if (showRatingNumber) {
-            this.ratingSpan.innerText = rating;
-        }
-        this.ratingStarsDiv.style.setProperty("--rating", rating);
-        if (uri) {
-            this.ratingLink.setAttribute('href', uri);
-        }
-        if (text) {
-            this.ratingLink.innerText = ratingsTotal + " " + text;
-        }
-    }
-
 }
 
-/*
-            :host {
-                display: inline-block;
-            }
-            
-            .rating {
-                font-family: var(--wertgarantie-rating-font-family, Roboto), sans-serif;
-                font-size: var(--wertgarantie-rating-font-size, 16px);
-            }
-
-            .rating__number {
-                color: var(--wertgarantie-rating-text-color, #2574be);
-                font-weight: var(--wertgarantie-rating-font-weight, 400)
-            }
-            
-            .rating__stars {
-                --rating: 1.3;
-                --star-empty: #d3dbdb;
-                --star-filled: var(--wertgarantie-rating-stars-color, #ee8a18);
-                --percent: calc(var(--rating) / 5 * 100%);
-                display: inline-block;
-                font-family: Arial, serif; /!* make sure ★ appears correctly *!/
-                background: linear-gradient(90deg, var(--star-filled) var(--percent), var(--star-empty) var(--percent));
-                -webkit-background-clip: text;
-                -webkit-text-fill-color: transparent;
-                letter-spacing: 3px;
-                font-size: var(--wertgarantie-rating-stars-font-size, inherit);
-            }
-
-            .rating__link {
-                color: var(--wertgarantie-rating-text-color, #2574be);
-                text-decoration: none;
-                font-weight: var(--wertgarantie-rating-font-weight, 400);
-            }*/
-
-
-
-
-
-
-
+customElements.define('wertgarantie-rating', WertgarantieRating);

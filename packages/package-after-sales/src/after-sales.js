d3ec60a9912621bf055c83bd36347677a095e401
henning <henning.nobbe@innoq.com>
2020-03-16 18:06:46 +0100

    Publish
     - wertgarantie-after-sales@1.0.3

--- packages/package-after-sales/src/after-sales.js
+++ packages/package-after-sales/src/after-sales.js
@@ -34,7 +34,7 @@ class WertgarantieAfterSales extends LitElement {
 
     constructor() {
         super();
-        this.componentVersion = '1.0.2';
+        this.componentVersion = '1.0.3';
         this.setProperties = this.setProperties.bind(this);
         this.renderOrder = this.renderOrder.bind(this);
         this.renderOrderItem = this.renderOrderItem.bind(this);

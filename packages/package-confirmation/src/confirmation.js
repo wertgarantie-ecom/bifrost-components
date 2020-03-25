c65de9fea5496e7b431c90e730f1c862099db77b
henning <henning.nobbe@innoq.com>
2020-03-25 10:55:02 +0100

    Publish
     - wertgarantie-after-sales@1.0.6
     - wertgarantie-confirmation@2.0.5
     - wertgarantie-selection-popup@2.0.9

--- packages/package-confirmation/src/confirmation.js
+++ packages/package-confirmation/src/confirmation.js
@@ -33,7 +33,7 @@ class WertgarantieConfirmation extends LitElement {
 
     constructor() {
         super();
-        this.componentVersion = '2.0.4';
+        this.componentVersion = '2.0.5';
 
         //method binding
         this.setProperties = this.setProperties.bind(this);

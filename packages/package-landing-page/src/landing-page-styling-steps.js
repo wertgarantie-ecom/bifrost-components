ab264534a25cc4b3573be6ac2bb2149f0ee85b68
arnel <arne.landwehr@innoq.com>
2020-04-01 13:30:28 +0200

    SWDBECOM-218 finish steps section

--- packages/package-landing-page/src/landing-page-styling-steps.js
+++ packages/package-landing-page/src/landing-page-styling-steps.js
@@ -1,14 +1,15 @@
 import {css} from "lit-element";
 
-// language=CSS
+//language=CSS
 export const landingPageStylingSteps = css`
     .steps {
         display: flex;
         justify-content: space-evenly;
         flex-wrap: nowrap;
         margin-top: 4em;
+        margin-bottom: 6em;
     }
-    
+
     .steps__step {
         display: flex;
         flex-flow: column;
@@ -17,7 +18,7 @@ export const landingPageStylingSteps = css`
         align-items: center;
         text-align: center;
     }
-    
+
     .step__square {
         width: 90px;
         height: 90px;
@@ -31,21 +32,39 @@ export const landingPageStylingSteps = css`
     }
 
     .icon__svg {
-        width: 50%;
-        height: 50%;
+        width: 40%;
+        height: 40%;
+    }
+
+    .steps__arrow {
+        width: 90px;
+        height: 90px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .steps__arrow--icon {
+        width: 14px;
+        height: 28px;
+        fill: var(--wertgarantie-landing-page-primary-color, #141414);
     }
 
     .icon__svg--colored {
         fill: var(--wertgarantie-landing-page-head-banner-title-color, white);
     }
-    
+
+    .icon__svg--test--background-colored {
+        fill: var(--wertgarantie-landing-page-primary-color, #141414);
+    }
+
     .step__title {
         color: var(--wertgarantie-landing-page-head-banner-subtitle-color, #353535);
         font-size: var(--wertgarantie-landing-page-head-banner-subtitle-font-size, 1.1em);
         font-weight: var(--wertgarantie-landing-page-head-banner-subtitle-font-weight, 600);
         margin: 2em 0;
     }
-    
+
     .step__text {
         font-size: 0.9em;
         line-height: 1.6em;
--- packages/package-landing-page/src/landing-page.js
+++ packages/package-landing-page/src/landing-page.js
@@ -1,6 +1,5 @@
 import {LitElement, html} from "lit-element";
 import fetchBifrost from "../../../shared-code/fetchBifrost";
-import {unsafeHTML} from "lit-html/directives/unsafe-html";
 import '../../package-rating/dist/rating.min.js';
 import {landingPageStylingGeneral} from "./landing-page-styling-general";
 import {landingPageStylingHead} from "./landing-page-styling-head";
@@ -9,7 +8,7 @@ import {landingPageStylingSteps} from "./landing-page-styling-steps";
 
 // import {landingPageStylingTariffCalculator} from "./landing-page-styling-tariff-calculator";
 
-class LandingPage extends LitElement{
+class LandingPage extends LitElement {
 
     static get styles() {
         return [
@@ -40,6 +39,7 @@ class LandingPage extends LitElement{
         this.displayComponent = this.displayComponent.bind(this);
         this.fetchLandingPageData = this.fetchLandingPageData.bind(this);
         this.setProperties = this.setProperties.bind(this);
+        this.renderArrowBetweenSteps = this.renderArrowBetweenSteps.bind(this);
     }
 
     connectedCallback() {
@@ -152,9 +152,7 @@ class LandingPage extends LitElement{
                                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
                                 </div>
                             </div>
-                            <div class="steps__arrow">
-                                
-                            </div>
+                           ${this.renderArrowBetweenSteps()}
                             <div class="steps__step step">
                                 <div class="step__square">
                                     2
@@ -166,9 +164,7 @@ class LandingPage extends LitElement{
                                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
                                 </div>
                             </div>
-                            <div class="steps__arrow">
-                            
-                            </div>
+                           ${this.renderArrowBetweenSteps()}
                             <div class="steps__step step">
                                 <div class="step__square">
                                     3
@@ -180,9 +176,7 @@ class LandingPage extends LitElement{
                                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
                                 </div>
                             </div>
-                            <div class="steps__arrow">
-                            
-                            </div>
+                           ${this.renderArrowBetweenSteps()}
                             <div class="steps__step step steps__step--final">
                                 <div class="step__square">
                                     <!-- Font Awesome Shield Icon -->
@@ -224,6 +218,18 @@ class LandingPage extends LitElement{
                 </div>
             </div>
         ` : html``;
+    }
+
+    renderArrowBetweenSteps() {
+        //language=HTML
+        return html`
+            <div class="steps__arrow">
+                <!-- Font Awesome chevron right: -->
+                <svg class="steps__arrow--icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                    <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/>
+                </svg>
+            </div>
+        `;
     }
 }
 

dfaecc674e24d925feb416c034c07ed8eb206afe
henning <henning.nobbe@innoq.com>
2020-04-03 12:40:09 +0200

    Publish
     - wertgarantie-landing-page@0.0.7

--- packages/package-landing-page/src/landing-page.js
+++ packages/package-landing-page/src/landing-page.js
@@ -44,7 +44,7 @@ class LandingPage extends LitElement {
 
     connectedCallback() {
         super.connectedCallback();
-        this.componentVersion = '0.0.6';
+        this.componentVersion = '0.0.7';
 
         this.bifrostUri = this.getAttribute("data-bifrost-uri") || "https://wertgarantie-bifrost-dev.herokuapp.com/wertgarantie";
         this.partnerNumber = this.getAttribute('data-partner-number');

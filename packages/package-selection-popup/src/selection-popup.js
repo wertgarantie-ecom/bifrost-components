469ddfafcf43247feaa6168a4f9fdd2027fb25ee
henning <henning.nobbe@innoq.com>
2020-05-06 16:22:57 +0200

    Publish
     - wertgarantie-selection-popup@2.0.14

--- packages/package-selection-popup/src/selection-popup.js
+++ packages/package-selection-popup/src/selection-popup.js
@@ -46,7 +46,7 @@ class WertgarantieSelectionPopUp extends LitElement {
     constructor() {
         super();
         this.initialized = false;
-        this.componentVersion = '2.0.13';
+        this.componentVersion = '2.0.14';
 
         // method binding
         this.setProperties = this.setProperties.bind(this);

80ead9b9c74558cd3c2343cf830e7e76b6e5884e
arnel <arne.landwehr@innoq.com>
2020-05-18 10:04:23 +0200

    Publish
     - wertgarantie-confirmation@2.0.22

--- packages/package-confirmation/src/confirmation.js
+++ packages/package-confirmation/src/confirmation.js
@@ -43,7 +43,7 @@ class WertgarantieConfirmation extends LitElement {
 
     constructor() {
         super();
-        this.componentVersion = '2.0.21';
+        this.componentVersion = '2.0.22';
 
         //method binding
         this.setProperties = this.setProperties.bind(this);

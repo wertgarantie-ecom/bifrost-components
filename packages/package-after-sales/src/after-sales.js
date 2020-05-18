2cec2b237bfa661d3966d36695feebc6761f51ee
arnel <arne.landwehr@innoq.com>
2020-05-18 20:35:29 +0200

    Publish
     - wertgarantie-after-sales@1.0.12
     - wertgarantie-integrations@0.0.6

--- packages/package-after-sales/src/after-sales.js
+++ packages/package-after-sales/src/after-sales.js
@@ -37,7 +37,7 @@ class WertgarantieAfterSales extends LitElement {
 
     constructor() {
         super();
-        this.componentVersion = '1.0.11';
+        this.componentVersion = '1.0.12';
         this.setProperties = this.setProperties.bind(this);
         this.renderOrder = this.renderOrder.bind(this);
         this.renderOrderItem = this.renderOrderItem.bind(this);

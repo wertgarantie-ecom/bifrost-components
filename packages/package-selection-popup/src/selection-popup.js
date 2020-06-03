8e4254649f2b8136c1b46f215ae6499f85face53
arnel <arne.landwehr@innoq.com>
2020-06-03 13:25:13 +0200

    Publish
     - wertgarantie-selection-popup@2.0.31

--- packages/package-selection-popup/src/selection-popup.js
+++ packages/package-selection-popup/src/selection-popup.js
@@ -48,7 +48,7 @@ class WertgarantieSelectionPopUp extends LitElement {
     constructor() {
         super();
         this.initialized = false;
-        this.componentVersion = '2.0.30';
+        this.componentVersion = '2.0.31';
 
         // method binding
         this.setProperties = this.setProperties.bind(this);

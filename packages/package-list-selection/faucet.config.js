5d8408f9c8f3f618fddd1627891b7a0b8a67674e
arnel <arne.landwehr@innoq.com>
2020-06-17 10:22:36 +0200

    SWDBECOM-340 add list component build and first fetch call

---
+++ packages/package-list-selection/faucet.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    js: [{
+        source: "./src/list-selection.js",
+        target: "./dist/list-selection.min.js",
+        compact: "mangle"
+    }],
+    watchDirs: ["./*"]
+};
--- packages/package-list-selection/src/list-selection.js
+++ packages/package-list-selection/src/list-selection.js
@@ -1,5 +1,7 @@
 import {LitElement, html} from "lit-element";
 import {listSelectionStyling} from "./listSelectionStyling";
+import fetchBifrost from "../../../shared-code/fetchBifrost";
+import initSentry from "../../../shared-code/sentry";
 
 class WertgarantieListSelection extends LitElement {
     static get styles() {
@@ -23,10 +25,51 @@ class WertgarantieListSelection extends LitElement {
     constructor() {
         super();
         this.componentVersion = '0.0.1';
+
+
+        this.displayComponent = this.displayComponent.bind(this);
+        this.setProperties = this.setProperties.bind(this);
+        this.checkDataComplete = this.checkDataComplete.bind(this);
+        this.fetchListSelectionComponentData = this.fetchListSelectionComponentData.bind(this);
     }
 
     connectedCallback() {
         super.connectedCallback();
+
+        this.clientId = this.getAttribute('data-client-id');
+        this.shopOrderBase64 = this.getAttribute('data-shop-order-base64');
+        this.bifrostUri = this.getAttribute('data-bifrost-uri') || "https://ecommerce.wertgarantie.com/wertgarantie";
+
+        this.displayComponent();
+    }
+
+    displayComponent() {
+        this.fetchListSelectionComponentData()
+            .then(this.checkDataComplete)
+            .then(this.setProperties)
+            .then(() => this.showComponent = true)
+            .catch(() => this.showComponent = false)
+    }
+
+    async fetchListSelectionComponentData() {
+        const url = new URL(`${this.bifrostUri}/ecommerce/clients/${this.clientId}/components/list-selection`);
+        const response = await fetchBifrost(url, 'PUT', this.componentVersion, {
+            shopShoppingCart: this.shopOrderBase64
+        });
+
+        if (response.status !== 200) {
+            return undefined;
+        }
+
+        return response.body;
+    }
+
+    setProperties() {
+
+    }
+
+    checkDataComplete() {
+        //TODO keep or delete?
     }
 
     render() {

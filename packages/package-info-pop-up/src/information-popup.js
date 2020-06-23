d049fff13ef2e895dec551bf6c2ebdaa3e692a8c
henning <henning.nobbe@innoq.com>
2020-06-23 11:12:51 +0200

    SWDBECOM-342 add new component package for info popup

---
+++ packages/package-info-pop-up/src/information-popup.js
@@ -0,0 +1,10 @@
+import {LitElement, html} from "lit-element";
+
+class WertgarantieInformationPopup extends LitElement {
+
+}
+
+
+if (!customElements.get('wertgarantie-information-popup')) {
+    customElements.define('wertgarantie-information-popup', WertgarantieInformationPopup);
+}

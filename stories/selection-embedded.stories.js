53acca9f7a412cb4e34a21852aeed064620baaec
arnel <arne.landwehr@innoq.com>
2020-10-08 17:04:44 +0200

    SWDBECOM-372 try to fix build

--- stories/selection-embedded.stories.js
+++ stories/selection-embedded.stories.js
@@ -1,21 +1,22 @@
 import {storiesOf} from '@storybook/html';
 import '../packages/package-rating/dist/rating.min.js';
-import 'wertgarantie-selection-embedded/dist/selection-embedded.min';
+import '../packages/package-selection-embedded/dist/selection-embedded.min';
 import selectionEmbeddedPhone from './documentation/selectionEmbedded/selectionEmbeddedPhone.md';
 import selectionEmbeddedBike from './documentation/selectionEmbedded/selectionEmbeddedBike.md';
 
 import showdown from 'showdown';
+
 showdown.setFlavor('github');
 var converter = new showdown.Converter();
 
 storiesOf('Components|Embedded Selection', module)
-.add('Embedded Selection Phone', () => `
+    .add('Embedded Selection Phone', () => `
     <div class="markdown-body"> 
         ${converter.makeHtml(selectionEmbeddedPhone)}
     </div>`
-)
-.add('Embedded Selection Bike/E-Bike', () => `
+    )
+    .add('Embedded Selection Bike/E-Bike', () => `
     <div class="markdown-body"> 
         ${converter.makeHtml(selectionEmbeddedBike)}
     </div>`
-);
+    );

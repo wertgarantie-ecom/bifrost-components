a4f5baec0477c097037d0555191e4d02489d31da
arnel <arne.landwehr@innoq.com>
2020-10-14 15:50:26 +0200

    Publish
     - wertgarantie-information-popup@1.0.8
     - wertgarantie-landing-page@0.0.21
     - wertgarantie-component-loader@1.0.8
     - wertgarantie-rating@2.0.16
     - wertgarantie-selection-embedded@0.0.30
     - wertgarantie-selection-popup@2.0.52

--- packages/package-selection-popup/src/selection-popup.js
+++ packages/package-selection-popup/src/selection-popup.js
@@ -49,7 +49,7 @@ class WertgarantieSelectionPopUp extends LitElement {
     constructor() {
         super();
         this.initialized = false;
-        this.componentVersion = '2.0.51';
+        this.componentVersion = '2.0.52';
 
         this.setProperties = this.setProperties.bind(this);
         this.fetchPolicy = this.fetchPolicy.bind(this);

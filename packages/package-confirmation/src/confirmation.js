6637665711d43598149f182958f13c3338bacc7e
henning <henning.nobbe@innoq.com>
2020-10-15 12:01:23 +0200

    Publish
     - wertgarantie-confirmation@2.0.40
     - wertgarantie-selection-embedded@0.0.31

--- packages/package-confirmation/src/confirmation.js
+++ packages/package-confirmation/src/confirmation.js
@@ -43,7 +43,7 @@ class WertgarantieConfirmation extends LitElement {
 
     constructor() {
         super();
-        this.componentVersion = '2.0.39';
+        this.componentVersion = '2.0.40';
 
         this.setProperties = this.setProperties.bind(this);
         this.checkStateOnSubmit = this.checkStateOnSubmit.bind(this);
